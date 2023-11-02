e41b8be588cf232cbe1b09b89137537fe22ac338
Inkeri <inkeri.ahlstrom@helsinki.fi>
2023-11-02 13:08:04 +0200

    testit

--- viikko1/nhl-statistics-1/src/tests/statistics_service_test.py
+++ viikko1/nhl-statistics-1/src/tests/statistics_service_test.py
@@ -20,36 +20,37 @@ class TestStatisticsService(unittest.TestCase):
         )
     
     def test_return_correct_points(self):
-        pelaaja = self.sort_by_points("Lemieux")
+        pelaaja = self.stats.search("Lemieux")
 
-        self.assertAlmostEqual(pelaaja, 45+54)
+        self.assertAlmostEqual(pelaaja.points, 99)
 
     def test_konstruktori_luo_pelaajat(self):
-        self.assertAlmostEqual(self.stats)
+        self.assertAlmostEqual(len(self.stats._players), 5)
     
     def test_pelaajan_haku_toimii(self):
         pelaaja = self.stats.search("Semenko")
 
-        self.assertAlmostEqual(pelaaja, "Semenko")
+        self.assertEqual(pelaaja.name, "Semenko")
     
     def test_pelaajan_haku_none(self):
         pelaaja = self.stats.search("Mksks")
 
-        self.assertAlmostEqual(pelaaja, None)
+        self.assertEqual(pelaaja, None)
     
     def test_list_of_players(self):
         joukkue = self.stats.team("EDM")
 
-        pelaajat = [Player("Semenko", "EDM", 4, 12),
-            Player("Kurri",   "EDM", 37, 53),
-            Player("Gretzky", "EDM", 35, 89)]
+        pelaajat = ["Semenko", "Kurri", "Gretzky"]
 
-        self.assertAlmostEqual(joukkue, pelaajat)
+        self.assertAlmostEqual([pelaaja.name for pelaaja in joukkue], pelaajat)
     
-    def test_top_players(self):
-        pass
+    def test_top_three_players(self):
+        top_three = self.stats.top(2)
 
-
+        result = [Player("Gretzky", "EDM", 35, 89),
+                  Player("Lemieux", "PIT", 45, 54),
+                  Player("Yzerman", "DET", 42, 56),]
         
+        self.assertListEqual(top_three, result)
     
     
