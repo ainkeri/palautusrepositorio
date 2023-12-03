35c23a6eda2f7aef681995710dded6eed48082b3
Inkeri <inkeri.ahlstrom@helsinki.fi>
2023-12-03 17:36:32 +0200

    Tehtävä 5

--- viikko5/laskin/src/sovelluslogiikka.py
+++ viikko5/laskin/src/sovelluslogiikka.py
@@ -1,17 +1,30 @@
 class Sovelluslogiikka:
     def __init__(self, arvo=0):
         self._arvo = arvo
+        self._edellinen_arvo = None
+    
+    def tallenna_tila(self):
+        self._edellinen_arvo = self._arvo
 
     def miinus(self, operandi):
-        self._arvo = self._arvo - operandi
+        self.tallenna_tila()
+        self._arvo -= operandi
 
     def plus(self, operandi):
-        self._arvo = self._arvo + operandi
+        self.tallenna_tila()
+        self._arvo += operandi
 
     def nollaa(self):
+        self.tallenna_tila()
         self._arvo = 0
+    
+    def kumoa(self):
+        if self._edellinen_arvo is not None:
+            self._arvo = self._edellinen_arvo
+            self._edellinen_arvo = None
 
     def aseta_arvo(self, arvo):
+        self.tallenna_tila()
         self._arvo = arvo
 
     def arvo(self):
